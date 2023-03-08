327021c996d58510ad0eb2b2310773fb180387fc
alexanderroshchin <alexander.roshchin85@gmail.com>
2023-03-08 17:27:15 +0200

    Created auth module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {
+  }
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (currentUser) => {
+        this.authService.setCurrentUser(currentUser);
+      },
+      error: (err) => {
+        console.log('err', err);
+        this.authService.setCurrentUser(null);
+      },
+  })
+  }
+
   title = 'trello';
 }
