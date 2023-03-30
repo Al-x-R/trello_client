a0cbc2653afd3454f85fa0839ed35a7d17bf825a
alexanderroshchin <alexander.roshchin85@gmail.com>
2023-03-30 20:15:50 +0300

    Added getting boards

---
+++ src/app/boards/boards.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuardService } from '../auth/services/authGuard.service';
+import { BoardsComponent } from './components/boards/boards.component';
+import { BoardsService } from '../shared/services/boards.service';
+
+const routes: Routes = [
+  {
+    path: 'boards',
+    component: BoardsComponent,
+    canActivate: [AuthGuardService],
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes), ],
+  declarations: [BoardsComponent],
+  providers: [BoardsService],
+})
+export class BoardsModule {
+}
